c32a7afdc199ee5ede6352870209eb1fcb4c2235
Andrey Savelyev <savelyev.andrey@gmail.com>
2016-02-13 15:15:39 +0300

    ()

--- lib/huevent_machine.rb
+++ lib/huevent_machine.rb
@@ -1,35 +1,12 @@
 require 'socket'
 
 class HueventMachine
-  module BaseServer
-    def initialize(machine)
-      @machine = machine
-    end
-  end
-
-  def initialize(addr, port, handler)
-    @addr, @port = addr, port
-    @handler_module = handler
-    @server_class = Class.new
-    @server_class.include BaseServer
-    @server_class.include @handler_module
-
-    @clients = []
-  end
-
-  def run
-    @handle = @server_class.new(self)
-    puts "RUN SERVER"
-    @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
-    @addrinfo = Addrinfo.tcp(@addr, @port)
-    @socket.bind(@addrinfo)
-    @socket.listen(5)
+  def self.run
+    yield
 
     loop do
       puts 'wait accept'
-      #client_socket, client_addrinfo = @socket.accept
-
-      read_sockets = clients + [@socket]
+      read_sockets = @clients + [@socket]
       ready = IO.select(read_sockets, [], [])
 
       ready[0].each do |io|
@@ -53,7 +30,6 @@ class HueventMachine
             io.close
             clients.delete(io)
           else
-
             puts "Client: #{str}"
           end
         end
@@ -62,15 +38,20 @@ class HueventMachine
   end
 
   def stop
-    #
-  end
-
-  def self.create_server(address, port, handler)
-    new(address, port, handler)
   end
 
   def self.start_server address, port, handler
-    server = create_server(address, port, handler)
-    server.run
+    @@servers ||= []
+
+    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+    addrinfo = Addrinfo.tcp(address, port)
+    socket.bind(addrinfo)
+    socket.listen(5)
+
+    server_class = Class.new
+    server_class.include handler
+    handle = server_class.new
+
+    @@servers << handle
   end
 end
--- test/huevent_machine_test.rb
+++ test/huevent_machine_test.rb
@@ -17,27 +17,16 @@ class TestHueventMachine < Minitest::Test
     @addr = '127.0.0.1'
     @port = rand(2224..2999)
     @handler = EchoServer
-    @machine = HueventMachine.new(@addr, @port, @handler)
-  end
-
-  def test_instance
-    assert @machine
   end
 
   def test_start_server
-    child_pid = Process.fork do
-      server = HueventMachine.create_server(@addr, @port, @handler)
-      server.run
-    end
-
-    sleep 2
+    server = HueventMachine.start_server(@addr, @port, @handler)
 
     socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
     addrinfo = Socket.pack_sockaddr_in(@port, @addr)
     assert_equal socket.connect(addrinfo), 0
+    assert_equal server.class_variable_get('@@servers'), 1
     socket.close
-
-    Process.kill("TERM", child_pid)
 
   end
 
