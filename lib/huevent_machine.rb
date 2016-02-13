211b587b5ef19346569333d3938a1fe1ca64c509
Nikolay Markov <mainameiz@gmail.com>
2016-02-13 14:25:26 +0300

    step2

--- lib/huevent_machine.rb
+++ lib/huevent_machine.rb
@@ -1,10 +1,29 @@
+require 'socket'
+
 class HueventMachine
-  def initialize(address, port, handler)
-    @address, @port = address, port
-    @handler = handler.new
+  def initialize(addr, port, handler)
+    @addr, @port = addr, port
+    @handler = handler
+  end
+
+  def run
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAa4'
+    @server = Class.new
+    @server.include @handler
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAa3'
+
+    @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+    @addrinfo = Addrinfo.tcp(@addr, @port)
+    @socket.bind(@addrinfo)
+    @socket.listen(5)
+
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAa'
   end
 
   def self.start_server address, port, handler
-    new(address, port, handler)
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAa1'
+    server = new(address, port, handler)
+    server.run
+    puts 'AAAAAAAAAAAAAAAAAAAAAAAAAAAa2'
   end
 end
--- test/huevent_machine_test.rb
+++ test/huevent_machine_test.rb
@@ -1,8 +1,14 @@
 require "minitest/autorun"
-require 'huevent_machine'
+require "huevent_machine"
+require "socket"
 
 
 class TestHueventMachine < Minitest::Test
+  module EchoServer
+    def post_init
+    end
+  end
+
   def setup
     @addr = 'localhost'
     @port = 2222
@@ -15,7 +21,20 @@ class TestHueventMachine < Minitest::Test
   end
 
   def test_start_server
-    assert @machine.start_server(@address, @port, @handler)
+    child_id = Process.fork do
+      puts 'qweqweeeeeeeeeeeeeeee'
+      puts @address
+      puts @port
+      puts @address
+      @machine.start_server(@address, @port, @handler)
+      puts 'qweqweeeeeeeeeeeeeeee2'
+    end
+
+    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+    addrinfo = Socket.pack_sockaddr_in(@port, @addr)
+    assert_equal socket.connect(addrinfo), 0
+
+    Process.wait
   end
 
 end
